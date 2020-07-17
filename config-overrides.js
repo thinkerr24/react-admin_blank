d9e97013ecb5c7952dd3b0810047ea0f5d04e348
thinkerr24 <thinkerr24@163.com>
2020-07-17 23:25:02 +0800

    002.2 antd custom themes

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 module.exports = override(
     // 针对antd实现按需打包: 根据import来打包(使用到babel-plugin-import)
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: 'css', // 自动打包相关的样式
+        style: true, // 自动打包相关的样式
+    }),
+    // 使用less-loader(v5.0.0)对源码中的less变量进行重新指定
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#1890ff' },
     }),
 );
