075d4e4b7973960ec5aa1fa4be9515291965b7b0
thinkerr24 <thinkerr24@163.com>
2020-10-21 22:50:25 +0800

    004.3 high-level function&component

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -61,10 +61,24 @@ class Login extends Component {
 
 /**
  * 1. 高阶函数
+        1). 一类特别的函数
+            a. 接受函数类型的参数
+            b. 返回值是函数
+        2). 常见
+            a. 定时器: setTimeout()/setInterval()
+            b. Promise: Promise(() => {}) then(value => {}, reason => {})
+            c. 数组遍历相关的方法: forEach()/filter()/map()/reduce()/find()/findIndex()
+            d. 函数对象的bind()
+            e. Form.create()() / getFieldDecorator()()
+        3). 高阶函数更新动态, 更加具有扩展性
  * 2. 高阶组件
+        1). 本质就是一个函数
+        2). 接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
+        3). 作用: 扩展组件的功能
+        4). 高阶组件也是高阶函数: 接收一个组件函数, 返回是一个新的组件函数
  */
 // 包装Form组件并生成一个新的组件: Form(Login)
-// 新组件会向
+// 新组件会向Form组件传递一个强大的对象属性: form
 const WrapLogin = Form.create()(Login);
 export default WrapLogin;
 
