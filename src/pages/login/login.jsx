e875c2dc6731b226d99df0b4cc911592283db311
thinkerr24 <thinkerr24@163.com>
2020-10-26 22:00:01 +0800

    004.6 unified validation

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -17,11 +17,22 @@ class Login extends Component {
     handleSubmit = event => {
         // 阻止事件的默认行为
         event.preventDefault();
+
+        // 对所有表单字段进行校验
+        this.props.form.validateFields((err, values) => {
+            // 校验成功
+            if (!err) {
+              console.log('提交登录的ajax请求', values);
+            } else {
+                console.log('校验失败!');
+            }
+        });
+
         // 得到form对象
-        const form = this.props.form;
+        // const form = this.props.form;
         // 获取表单项的输入数据
-        const values = form.getFieldsValue();
-        console.log('handleSubmit:', values);
+        // const values = form.getFieldsValue();
+        // console.log('handleSubmit:', values);
     }
 
     // 对密码进行自定义验证
