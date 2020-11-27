7461564dbaa1305ed51aba60df0dcfcc13d2ea99
thinkerr24 <thinkerr24@163.com>
2020-11-27 22:33:02 +0800

    040.动态显示当前时间和天气

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,7 @@ export const reqWeather = city => new Promise((resolve, reject) => {
     jsonp(url, {}, (err, data) => {
         // console.log("jsonp()", err, data);
         if (!err && data.status === 'success') {
-            const { dayPictureUrl, weather } = data.result[0].weather_data[0];
+            const { dayPictureUrl, weather } = data.results[0].weather_data[0];
             resolve({ dayPictureUrl, weather });
         } else {
             message.error('获取天气信息失败!');
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { reqWeather } from '../../api';
+import memoryUtils from '../../utils/memoryUtils';
 import "./index.less";
 /**
  * 头部组件
  */
 export default class Header extends Component {
+    state = {
+        currentTime: moment(new Date()).format('yyyy-MM-DD HH:mm:ss'),   // 当前时间
+        dayPictureUrl: '', // 天气图标
+        weather: '' // 天气
+    }
+
+    getTime = () => {
+        // 每隔1s获取当前时间, 并更新状态数据currentTime
+        setInterval(() => {
+            const currentTime = moment(new Date()).format('yyyy-MM-DD HH:mm:ss');
+            this.setState({ currentTime });
+        }, 1000);
+    }
+
+    getWeather = async () => {
+        const {dayPictureUrl, weather} = await reqWeather('武汉');
+        this.setState({dayPictureUrl, weather}); 
+    }
+
+    // 第一次render之后执行
+    // 一般在此执行异步操作: 发ajax/启动定时器
+    componentDidMount() {
+        // 获取当前时间
+        this.getTime();
+
+        // 获取当前天气显示
+        this.getWeather();
+    }
+
     render() {
+
+        const { currentTime, dayPictureUrl, weather } = this.state;
+        const username = memoryUtils.user.username;
         return (
             <div className="header">
                 <div className="header-top">
-                    <span>欢迎, admin</span>
+                    <span>欢迎, {username}</span>
                     <a>退出</a>
                 </div>
                 <div className="header-bottom">
@@ -16,9 +51,9 @@ export default class Header extends Component {
                         首页
                     </div>
                     <div className="header-bottom-right">
-                        <span>2020-11-27 21:09:02</span>
-                        <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="weather"/>
-                        <span>晴</span>
+                        <span>{currentTime}</span>
+                        <img src={dayPictureUrl} alt="weather" />
+                        <span>{weather}</span>
                     </div>
                 </div>
             </div>
