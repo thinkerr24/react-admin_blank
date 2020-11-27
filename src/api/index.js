976c5cf9e42e49333f3240e13231b40fd72120a4
thinkerr24 <thinkerr24@163.com>
2020-11-27 21:59:15 +0800

    038.定义jsonp请求的接口请求函数

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,8 @@
 包含应用中所有接口请求函数的模块
 每个函数的返回值都是Promise
  */
+import { message } from 'antd';
+import jsonp from 'jsonp';
 import ajax from './ajax';
 
 // 跨域问题(不一致):主机名 协议类型 端口号 解决: 1.JOSNP(only GET request) 2.CORS(back-end config) 3.proxy 
@@ -12,7 +14,21 @@ import ajax from './ajax';
 //     return ajax('/login', {username, password}, 'POST');
 // }
 
-export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST');
+export const reqLogin = (username, password) => ajax('/login', { username, password }, 'POST');
 
 // 添加用户
 export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST');
+
+// jsonp请求接口的请求函数
+export const reqWeather = city => new Promise((resolve, reject) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`;
+    jsonp(url, {}, (err, data) => {
+        // console.log("jsonp()", err, data);
+        if (!err && data.status === 'success') {
+            const { dayPictureUrl, weather } = data.result[0].weather_data[0];
+            resolve({ dayPictureUrl, weather });
+        } else {
+            message.error('获取天气信息失败!');
+        }
+    });
+});
